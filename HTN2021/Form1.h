85ce515a78f2060bc6bd633b4f9d3a1fb975eaf9
Stephenwang3801 <stephenwang3801@gmail.com>
2021-01-16 17:20:50 -0500

    GUI creation draft

--- HTN2021/Form1.h
+++ HTN2021/Form1.h
@@ -35,6 +35,43 @@ namespace CppCLRWinformsProjekt {
 			}
 		}
 
+
+
+
+	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
+	private: System::Windows::Forms::PictureBox^ pictureBox11;
+	private: System::Windows::Forms::PictureBox^ pictureBox12;
+	private: System::Windows::Forms::PictureBox^ pictureBox4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
 	private:
 		/// <summary>
 		/// Erforderliche Designervariable.
@@ -46,14 +83,65 @@ namespace CppCLRWinformsProjekt {
 		/// Erforderliche Methode für die Designerunterstützung.
 		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
 		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+		void InitializeComponent(void){
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->pictureBox11 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox12 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox11))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox12))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox11
+			// 
+			this->pictureBox11->Location = System::Drawing::Point(0, 395);
+			this->pictureBox11->Name = L"pictureBox11";
+			this->pictureBox11->Size = System::Drawing::Size(140, 10);
+			this->pictureBox11->TabIndex = 10;
+			this->pictureBox11->TabStop = false;
+			// 
+			// pictureBox12
+			// 
+			this->pictureBox12->Location = System::Drawing::Point(180, 395);
+			this->pictureBox12->Name = L"pictureBox12";
+			this->pictureBox12->Size = System::Drawing::Size(140, 10);
+			this->pictureBox12->TabIndex = 11;
+			this->pictureBox12->TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this->pictureBox4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox4.Image")));
+			this->pictureBox4->Location = System::Drawing::Point(0, 480);
+			this->pictureBox4->Name = L"pictureBox4";
+			this->pictureBox4->Size = System::Drawing::Size(320, 320);
+			this->pictureBox4->TabIndex = 3;
+			this->pictureBox4->TabStop = false;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(800, 800);
+			this->Controls->Add(this->pictureBox12);
+			this->Controls->Add(this->pictureBox11);
+			this->Controls->Add(this->pictureBox4);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox11))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox12))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+
+	}
+	private: System::Void button1_Click_1(System::Object^ sender, System::EventArgs^ e) { }
+	private: System::Void pictureBox5_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
--- HTN2021/pch.h
+++ HTN2021/pch.h
@@ -7,6 +7,6 @@
 #ifndef PCH_H
 #define PCH_H
 
-// Fügen Sie hier Header hinzu, die vorkompiliert werden sollen.
+// Add headers here to be precompiled.
 
 #endif //PCH_H
